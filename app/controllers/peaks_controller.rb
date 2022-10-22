61626477f6826ed05b4ee63aefb8c9f824f0958a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-22 15:24:14 -0700

    Adds create action to peaks_controller

--- app/controllers/peaks_controller.rb
+++ app/controllers/peaks_controller.rb
@@ -1,11 +1,32 @@
 class PeaksController < ApplicationController
+  before_action :set_peak
+
   def index
     @peaks = Peak.all
     render template: "peaks/index"
   end
 
   def show
-    @peak = Peak.find_by(id: params[:id])
     render template: "peaks/show"
+  end
+
+  def create
+    @peak = Peak.new(peak_params)
+    if @peak.save
+      Image.create(url: params["image_url"], peak_id: @peak.id)
+      render template: "peak/show"
+    else
+      render json: { errors: @peak.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_peak
+    @peak = Peak.find(params[:id])
+  end
+
+  def peak_params
+    params.require(:peak).permit(:id, :name, :location, :elevation, :description)
   end
 end
